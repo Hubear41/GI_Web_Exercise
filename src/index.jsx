b41f1b707a1bc324036b91ed3ecf1bf2d62ecd83
Dennis Hu <ddh241@gmail.com>
2019-11-09 15:36:55 -0500

    complete webpack setup

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styles from './styles/entry.scss';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById('root');
+
+    ReactDOM.render(<h1>Test</h1>, root);
+});
