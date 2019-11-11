e4207e4a96a9297de31ba320f322dc3767453286
Dennis Hu <ddh241@gmail.com>
2019-11-11 17:29:27 -0500

    clean up

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,14 +5,8 @@ import Dropdown from '../dropdown/dropdown';
 import data from '../../assets/data/content.json';
 import imageIndex from '../../util/image_index';
 
-const dropdownContent = () => {
-    const labels = {
-        products: data.filter(el => el.label === "Products")[0].subnavList,
-        industries: data.filter( el => el.label === "Industries")[0].subnavList,
-    }
-
-    return { getValue: string => labels[string] };
-}
+const products = data.filter(el => el.label === "Products")[0].subnavList;
+const industries = data.filter(el => el.label === "Industries")[0].subnavList;
 
 // create a custom mergedSetState method
 const useMergedState = initialState => {
@@ -26,22 +20,20 @@ const useMergedState = initialState => {
 }
 
 const Navbar = () => {
-    // const [dropdown, updateDropdown] = useMergedState({ type: null, hidden: true });
-    const [dropdown, updateDropdown] = useMergedState({ type: "products", hidden: false });
-    const content = dropdownContent();
+    const [dropdown, updateDropdown] = useMergedState({ type: null, hidden: true });
 
     let dropdownComponent = null;
     if (!dropdown.hidden) {
         if (dropdown.type === "products") {
             dropdownComponent = <Dropdown 
                                     type="PRODUCTS" 
-                                    subnavList={content.getValue("products")} 
+                                    subnavList={products} 
                                     updateDropdown={updateDropdown}
                                 />;
         } else if (dropdown.type === "industries") {
             dropdownComponent = <Dropdown 
                                     type="INDUSTRIES" 
-                                    subnavList={content.getValue("industries")} 
+                                    subnavList={industries} 
                                     updateDropdown={updateDropdown}
                                 />;
         }
